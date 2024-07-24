e93e0ea85a0d29d1a22ec385454806582e328bc1
tiomashimon <tiomashimon@gmail.com>
2024-07-24 18:05:12 -0400

    feat: Added User app with serializers, views and urls with tests

---
+++ app/core/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name', 'is_active', 'is_staff', 'last_login']
+    search_fields = ['email', 'name']
+    list_filter = ['is_active', 'is_staff', 'is_superuser']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser'),
+        }),
+    )
+    readonly_fields = ['last_login']
+
+admin.site.register(User, UserAdmin)
